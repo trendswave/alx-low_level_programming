e1ae44d584f15e2e9ea9811ba20244b2e16d6da2
trendswave <paulchinedumicheal@gmail.com>
2023-07-19 14:08:40 -0700

    prin last digits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - Computes the last digit of a number
+ *0n : The number
+ * Return:last digit  (Sucess)
+ */
+/*betty style*/
+int print_last_digit(int n)
+{
+	int i;
+
+	i = n % 10;
+
+	return (i);
+}
+
