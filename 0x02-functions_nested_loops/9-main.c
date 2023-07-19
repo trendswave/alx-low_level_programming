d9d5d691ffe297cf9a0d6f3248ebab9e0be85c1e
trendswave <paulchinedumicheal@gmail.com>
2023-07-19 15:18:31 -0700

    times table

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    times_table();
-    return (0);
+	times_table();
+	return (0);
 }
