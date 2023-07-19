709caac3e1db428136068b956e4c6f7253d8ed61
trendswave <paulchinedumicheal@gmail.com>
2023-07-19 13:27:13 -0700

    1-alphabt.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+}
+
