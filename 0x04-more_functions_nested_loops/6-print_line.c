9a88863de0424a0d693ccda7611a7a4659f6c897
trendswave <paulchinedumicheal@gmail.com>
2023-07-20 21:18:27 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - prints a line a given number of times
+ * @n: number of lines
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
