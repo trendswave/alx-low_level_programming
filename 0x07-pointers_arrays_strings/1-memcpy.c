2205bae10082fc0fd9768e072b34ac86cc379d35
trendswave <paulchinedumicheal@gmail.com>
2023-07-31 13:04:23 -0700

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    char *sptr = src;
+    char *dptr = dest;
+    unsigned int i = 0;
+    for (i = 0; i < n; i++) {
+        *dptr++ = *sptr++;
+    }
+    return dest;
+}
