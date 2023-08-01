e6d99d73437b85ce645a680ebc58088b85c1ad2f
trendswave <paulchinedumicheal@gmail.com>
2023-07-31 20:27:57 -0700

    8-print_diagsums

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
-char *_memcpy(char *dest, char *src, unsigned int n) {
-    char *sptr = src;
-    char *dptr = dest;
-    unsigned int i = 0;
-    for (i = 0; i < n; i++) {
-        *dptr++ = *sptr++;
-    }
-    return dest;
-}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *sptr = src;
+	char *dptr = dest;
+	unsigned int i = 0;
+	for (i = 0; i < n; i++){
+		*dptr++ = *sptr++;
+		}
+		return dest;
+		}
