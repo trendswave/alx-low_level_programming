730fc041621eb430f72ba1de1ea14cc39c28925c
trendswave <paulchinedumicheal@gmail.com>
2023-07-31 20:48:53 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,5 +15,5 @@ char *_strstr(char *, char *);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *, int);
 void set_string(char **s, char *to);
-
+void _puts(char *str);
 #endif /*MAIN_H*/
