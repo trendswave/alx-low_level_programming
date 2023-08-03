0a374fad6b4b6b708d89ffaabfc8d2253fa4818d
trendswave <paulchinedumicheal@gmail.com>
2023-08-02 21:00:07 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * helper - This function helps calculate whether n is prime
+ * @n: The integer to test
+ * @i: The integer to help calculate whether n is prime
+ * Return: 1 if n is prime, otherwise 0
+ */
+
+int helper(int n, int i)
+{
+	/* if n is greater than or equal to 25 */
+	if (n >= (i * i))
+	{
+		/* skip it's multiples and even numbers after 5 */
+		if (n % i == 0 || n % (i + 2) == 0)
+			return (0);
+		/* check primes after 11 */
+		i = i + 6;
+	}
+	return (1);
+}
+
+
+
+/**
+ * is_prime_number - This function returns a 1 if input integer is prime number
+ * @n: The integer to test
+ * Return: 1 if the n is prime, otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	/* if less than 1, not a prime */
+	if (n <= 1)
+		return (0);
+	/* 2 and 3 are prime */
+	else if (n <= 3)
+		return (1);
+	/* if factors of 2 or 3, not really  a prime */
+	else if (n % 2 == 0 || n % 3 == 0)
+		return (0);
+	/* check after 5 */
+	else
+		return (helper(n, 5));
+}
