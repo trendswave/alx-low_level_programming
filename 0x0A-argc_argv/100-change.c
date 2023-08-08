12e8c46de030be157780f8771457355899b6d6e7
trendswave <paulchinedumicheal@gmail.com>
2023-08-08 11:43:16 -0700

    program that prints the minimum number of coins to make change for an amount of money updated

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,56 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
-* main - Calculates the minimum number of coins needed for change.
-* @argc: Argument count.
-* @argv: Argument vector.
-*
-* Return: 0 on success, 1 on error.
-*/
-int main(int argc, char *argv[]);
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
 {
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
-		}
+	}
 
-		int cents = atoi(argv[1]);
+	num = atoi(argv[1]);
+	result = 0;
 
-		if (cents < 0)
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
 		{
-			printf("0\n");
-			return (0);
-			}
+			result++;
+			num -= coins[j];
+		}
+	}
 
-			int coins = 0;
-
-			while (cents >= 25)
-			{
-				cents -= 25;
-				coins++;
-				}
-				while (cents >= 10)
-				{
-					cents -= 10;
-					coins++;
-
-					while (cents >= 5)
-					{
-						cents -= 5;
-						coins++;
-						}
-						while (cents >= 2)
-						{cents -= 2;
-						coins++;
-						while (cents >= 1)
-						{
-							cents -= 1;
-							coins++;
-							}
-
-							printf("%d\n", coins);
-
-							return (0);
-						}
+	printf("%d\n", result);
+	return (0);
+}
