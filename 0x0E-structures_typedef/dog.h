7b5bc8d6dd21e9e4e85e47a70d359b31896dbcea
trendswave <paulchinedumicheal@gmail.com>
2023-08-14 19:48:45 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - This struct creates a new type
+ * @name: First member, char pointer
+ * @age: Second member, float
+ * @owner: Third memeber, char pointer
+ *
+ * Description: This struct has three elements and is a new type dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog - Typdef for dog type
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* _DOG_H_ by @trendswave*/
