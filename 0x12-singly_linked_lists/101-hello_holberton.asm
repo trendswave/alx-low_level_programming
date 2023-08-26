6e131db4893db43f55067ef766cbec29c8c3f537
trendswave <paulchinedumicheal@gmail.com>
2023-08-25 22:01:04 -0700

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,9 @@
-section .data
-	msg db	"FUTO, has been a greate school", `\n`
-
-	section .text
-	global main
+global   main
+	  extern    printf
 main:
-	mov     rax, 1
-	mov     rdi, 1
-	mov     rsi, msg
-	mov     rdx, 17
-	syscall
-	mov    rax, 60
-	mov    rdi, 0
-	syscall
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,
