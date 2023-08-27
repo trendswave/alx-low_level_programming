139c2510a960125aee5d900a650adc9aedfc901f
trendswave <paulchinedumicheal@gmail.com>
2023-08-27 07:54:09 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,15 @@
-global   main
-	  extern    printf
+section .data
+	msg db	"Hello, Holberton", `\n`
+
+	section .text
+	global main
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov   eax, 0
-	  ret
-format: db `Hello, Holberton\n`,
+	mov     rax, 1
+	mov     rdi, 1
+	mov     rsi, msg
+	mov     rdx, 17
+	syscall
+	mov    rax, 60
+	mov    rdi, 0
+	syscall
+
