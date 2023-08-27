41c7e8cf8f65fa96407a5c2ace9816283eee2084
trendswave <paulchinedumicheal@gmail.com>
2023-08-27 08:03:23 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,107 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_i - This function prints integers
- * @list: List of arguments
- * @sep: Separator
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
-
-void print_i(va_list list, char *sep)
-{
-	printf("%s%d", sep, va_arg(list, int));
-}
-
-/**
- * print_c - This function prints characters
- * @list: Character arguments
- * @sep: Separator
- */
-
-void print_c(va_list list, char *sep)
-{
-	printf("%s%c", sep, va_arg(list, int));
-}
-
-/**
- * print_s - This function prints strings
- * @list: List of arguments
- * @sep: Separator
- */
-
-void print_s(va_list list, char *sep)
-{
-	char *s;
-
-	/* assign string to pointer */
-	s = va_arg(list, char *);
-
-	if (s == NULL)
-		s = "(nil)";
-
-	printf("%s%s", sep, s);
-}
-
-/**
- * print_f - This function prints floats
- * @list: List of arguments
- * @sep: Separator
- */
-
-void print_f(va_list list, char *sep)
-{
-	printf("%s%f", sep, va_arg(list, double));
-}
-
-/**
- * print_all - This function prints out all types of specifiers
- * @format: The list of variable arguments
- */
-
 void print_all(const char * const format, ...)
 {
-	va_list ap;
-	char *sep;
-	int i, j;
+	int i = 0;
+	char *str, *sep = "";
 
-	/* store format specifier types in an array */
-	fm_t fm[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"f", print_f},
-		{"s", print_s},
-		{NULL, NULL}
-	};
+	va_list list;
 
-	/* initialize list */
-	va_start(ap, format);
+	va_start(list, format);
 
-	/* initialize variables */
-	i = 0;
-	sep = "";
-
-	/* loop through format */
-	while (format != NULL && format[i] != '\0')
+	if (format)
 	{
-		j = 0;
-
-		while (j < 4)
+		while (format[i])
 		{
-			if (format[i] == *(fm[j]).fm)
+			switch (format[i])
 			{
-				fm[j].p(ap, sep);
-				sep = ", ";
-
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
 			}
-			j++;
+			sep = ", ";
+			i++;
 		}
-		i++;
 	}
 
-	/* add newline */
 	printf("\n");
-
-	/* free memory */
-	va_end(ap);
+	va_end(list);
 }
