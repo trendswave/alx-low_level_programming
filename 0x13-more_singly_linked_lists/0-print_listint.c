7a47f0dbc71b0f9562bf5ab1a77f8042bb395f99
trendswave <paulchinedumicheal@gmail.com>
2023-08-28 06:11:35 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,20 @@
-#include "list.h"
-#include <stdio.h>
-#include <stddef.h>
+#include "lists.h"
 
 /**
-*print_listint  -This function prints all tghe element of a listint_t list
-*@h: The pointer to the struct listint_t
-*Return: The number of nodes
+* print_listint - This function prints all the elements of a listint_t list
+* @h: The pointer to the struct listint_t
+* Return: The number of nodes
+*by @trendswave
 */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t node =0;
+	size_t node = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n" , h->n);
+		printf("%d\n", h->n);
 		h = h->next;
-		node++
+		node++;
 	}
-	return (node);
+	return(node);
 }
