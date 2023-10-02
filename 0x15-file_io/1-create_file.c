2a147bc97e0b79618e8b0f3ace919608746555e9
Trendswave <paulchinedumicheal@gmail.com>
2023-10-02 14:40:27 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,15 +10,15 @@ int create_file(const char *filename, char *text_content)
 	int fid, j, res;
 
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 	fid = open(filename, O_WRONLY | O_CREAT  | O_TRUNC, 0600);
 	if (fid == -1)
-		return(-1);
+		return (-1);
 	if (text_content == NULL)
-		return(1);
+		return (1);
 	for(j = 0; text_content[j] != 0; j++)
 		;
 	res = write(fid, text_content, j);
 	close(fid);
-	return(res == -1 ? -1 : 1);
+	return (res == -1 ? -1 : 1);
 }
