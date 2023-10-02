9e6d748bf1e0b419f6c6066e7aff05ea517f7340
Trendswave <paulchinedumicheal@gmail.com>
2023-10-02 15:05:25 -0700

    betty checks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *createe_file - creates a file
+ *create_file - creates a file with pemission and content
  *@filename: Name of thne file to create
  *@text_content:NULL terminated string to write to the file
- *Return : 1 0n success, -1 on failure
+ *Return: If the function fails  -1, 1 on success.
  **/
 int create_file(const char *filename, char *text_content)
 {
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
-	for(j = 0; text_content[j] != 0; j++)
+	for (j = 0; text_content[j] != 0; j++)
 		;
 	res = write(fid, text_content, j);
 	close(fid);
