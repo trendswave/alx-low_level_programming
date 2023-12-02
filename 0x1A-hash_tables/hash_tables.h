f14f724a40f1bde8268c395d7e3c2c4fc2f004fe
trendswave <paulchinedumicheal@gmail.com>
2023-12-02 04:27:47 +0100

    Updating the header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -28,7 +28,7 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
-
+char *hash_table_get(const hash_table_t *ht, const char *key);
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
